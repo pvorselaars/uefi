9f6dcc0c3e16f307b8742eee7cdaf2adc53bb1c2
Perry Vorselaars <p.vorselaars@gmail.com>
2023-03-24 14:00:25 +0100

    Simple number printing

---
+++ examples/0x1-numbers.c
@@ -0,0 +1,83 @@
+#include "uefi.h"
+
+#define BASE_16	    16
+#define BASE_10 	10
+#define BASE_2		2
+#define NEWLINE 	L"\r\n"
+
+EFI_STATUS Reverse(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Con, CHAR16* buffer, INTN index){
+
+	INTN i = 0;
+	CHAR16 reverse[128];
+
+	while(index >= 0)
+		reverse[i++] = buffer[index--];
+
+	reverse[i] = 0;
+	
+	return Con->OutputString(Con, reverse);
+}
+
+EFI_STATUS IntToUnicode(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Con, INTN Value, INTN Base){
+
+	CHAR16 buffer[128];
+	BOOLEAN negative = FALSE;
+	INTN i = 0;
+
+	if((Value < 0) && Base == BASE_10){
+		negative = TRUE;
+		Value *= -1;
+	}
+
+	do {
+		const CHAR16 digit = (CHAR16)(Value % Base);
+		buffer[i++] = digit < 10 ? '0' + digit : 'a' + digit - 10;
+		Value /= Base;
+	} while (Value && i < 126);
+
+	if(negative)
+		buffer[i++] = '-';
+
+	switch(Base){
+		case BASE_2:
+			buffer[i++] = 'b';
+			buffer[i++] = '0';
+			break;
+		case BASE_16:
+			buffer[i++] = 'x';
+			buffer[i++] = '0';
+			break;
+	}
+
+	return Reverse(Con, buffer, i-1);
+}
+
+EFI_STATUS OutputInt(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Con, INTN Value, INTN Base){
+
+	return IntToUnicode(Con, Value, Base);
+}
+
+EFI_STATUS OutputNewline(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* Con){
+	return Con->OutputString(Con, NEWLINE);
+}
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable){
+
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* StdOut = SystemTable->ConOut;
+
+	OutputInt(StdOut, -100, BASE_10);
+	OutputNewline(StdOut);
+	OutputInt(StdOut, 100, BASE_10);
+	OutputNewline(StdOut);
+	OutputInt(StdOut, 12345, BASE_10);
+	OutputNewline(StdOut);
+	OutputInt(StdOut, 5, BASE_2);
+	OutputNewline(StdOut);
+	OutputInt(StdOut, 0x1234, BASE_16);
+	OutputNewline(StdOut);
+	OutputInt(StdOut, 0xabcd, BASE_16);
+	OutputNewline(StdOut);
+
+	return EFI_SUCCESS;
+}
+
--- uefi.h
+++ uefi.h
@@ -40,32 +40,41 @@ typedef VOID*			EFI_HANDLE;
 
 // Simple Text Protocol
 
-#define EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_GUID { 0x387477c2, 0x69c7, 0x11d2, {0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b }}
+#define EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_GUID { 0x387477c2, 0x69c7, 0x11d2, { 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b }}
 
-struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_RESET)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN BOOLEAN ExtendedVerification);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN CHAR16 *String);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_TEST_STRING)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN CHAR16 *String);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_QUERY_MODE)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ModeNumber, OUT UINTN *Columns, OUT UINTN *Rows);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_MODE)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ModeNumber);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_ATTRIBUTE)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ATTRIBUTE);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_CLEAR_SCREEN)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_CURSOR_POSITION)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN Column, IN UINTN Row);
-typedef EFI_STATUS (EFIAPI *EFI_TEXT_ENABLE_CURSOR)(IN struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN BOOLEAN Visible);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_RESET)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN BOOLEAN ExtendedVerification);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN CHAR16 *String);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_TEST_STRING)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN CHAR16 *String);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_QUERY_MODE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ModeNumber, OUT UINTN *Columns, OUT UINTN *Rows);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_MODE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ModeNumber);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_ATTRIBUTE)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN ATTRIBUTE);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_CLEAR_SCREEN)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_SET_CURSOR_POSITION)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN UINTN Column, IN UINTN Row);
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_ENABLE_CURSOR)(IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, IN BOOLEAN Visible);
 
-typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
-    UINT64		Reset;
-    EFI_TEXT_STRING 	OutputString;
-    UINT64		TestString;
-    UINT64	 	QueryMode;
-    UINT64		SetMode;
-    UINT64		SetAttribute;
-    UINT64		ClearScreen;
-    UINT64		SetCursorPosition;
-    UINT64		EnableCursor;
-    UINT64		Mode;
-} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_MODE {
+        INT32   MaxMode;
+        INT32   Mode;
+        INT32   Attribute;
+        INT32   CursorColumn;
+        INT32   CursorRow;
+        BOOLEAN CursorVisible;
+} EFI_SIMPLE_TEXT_OUTPUT_MODE;
+
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
+        EFI_TEXT_RESET		        Reset;
+        EFI_TEXT_STRING			OutputString;
+        EFI_TEXT_TEST_STRING		TestString;
+        EFI_TEXT_QUERY_MODE		QueryMode;
+        EFI_TEXT_SET_MODE		SetMode;
+        EFI_TEXT_SET_ATTRIBUTE		SetAttribute;
+        EFI_TEXT_CLEAR_SCREEN		ClearScreen;
+        EFI_TEXT_SET_CURSOR_POSITION	SetCursorPosition;
+        EFI_TEXT_ENABLE_CURSOR		EnableCursor;
+	EFI_SIMPLE_TEXT_OUTPUT_MODE	Mode;
+};
 
 // TABLES
 
@@ -88,27 +97,27 @@ typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
 #define EFI_SYSTEM_TABLE_REVISION	EFI_2_100_SYSTEM_TABLE_REVISION
 
 typedef struct EFI_TABLE_HEADER {
-    UINT64 Signature;
-    UINT32 Revision;
-    UINT32 HeaderSize;
-    UINT32 CRC32;
-    UINT32 Reserved;
+        UINT64 Signature;
+        UINT32 Revision;
+        UINT32 HeaderSize;
+        UINT32 CRC32;
+        UINT32 Reserved;
 } EFI_TABLE_HEADER;
 
 typedef struct EFI_SYSTEM_TABLE {
-    EFI_TABLE_HEADER			Hdr;
-    CHAR16*				FirmwareVendor;
-    UINT32 				FirmwareRevision;
-    EFI_HANDLE		                ConsoleInHandle;
-    UINT64		                ConIn;
-    EFI_HANDLE		                ConsoleOutHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL*    ConOut;
-    EFI_HANDLE		         	StandardErrorHandle;
-    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL*	StdErr;
-    UINT64		            	RuntimeServices;
-    UINT64				BootServices;
-    UINT64				NumberOfTableEntries;
-    UINT64				ConfigurationTable;
+        EFI_TABLE_HEADER		        Hdr;
+        CHAR16*				        FirmwareVendor;
+        UINT32 				        FirmwareRevision;
+        EFI_HANDLE		                ConsoleInHandle;
+        UINT64		                        ConIn;
+        EFI_HANDLE		                ConsoleOutHandle;
+        EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL*	ConOut;
+        EFI_HANDLE		         	StandardErrorHandle;
+        EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL*	StdErr;
+        UINT64		            	        RuntimeServices;
+        UINT64				        BootServices;
+        UINT64				        NumberOfTableEntries;
+        UINT64				        ConfigurationTable;
 } EFI_SYSTEM_TABLE;
 
 #endif
